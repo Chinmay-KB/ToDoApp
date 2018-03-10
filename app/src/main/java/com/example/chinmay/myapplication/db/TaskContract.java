aed111dad3f906b0017f68cadedd5ab4c6062595
Chinmay Kabi <chinmay.kabi@live.com>
2018-03-11 02:53:56 +0530

    This is my first commit

---
+++ app/src/main/java/com/example/chinmay/myapplication/db/TaskContract.java
@@ -0,0 +1,17 @@
+package com.example.chinmay.myapplication.db;
+import android.provider.BaseColumns;
+/**
+ * Created by Chinmay on 07-03-2018.
+ */
+
+
+public class TaskContract {
+    public static final String DB_NAME = "com.example.chinmay.myapplication.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
